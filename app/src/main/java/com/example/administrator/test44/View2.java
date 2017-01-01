5fd9adad338192ddc88028725999f1d42e48ce48
saizongyin <1040025753@qq.com>
2017-01-01 22:45:41 +0800

    修改view5

--- app/src/main/java/com/example/administrator/test44/View2.java
+++ app/src/main/java/com/example/administrator/test44/View2.java
@@ -4,5 +4,6 @@ package com.example.administrator.test44;
  * Created by Administrator on 2017/1/1.
  */
 public class View2 {
+    private String name = "haha";
     public int age = 3;
 }
--- app/src/main/java/com/example/administrator/test44/View5.java
+++ app/src/main/java/com/example/administrator/test44/View5.java
@@ -4,5 +4,6 @@ package com.example.administrator.test44;
  * Created by Administrator on 2017/1/1.
  */
 public class View5 {
+    private int age = 119;
     private int name = 4;
 }
